6e37b3b3e2d225cec9b74c1940009c41a827727a
Yurat <yurat.bekmuhamedov@yandex.com>
2022-09-24 16:29:44 +0500

    Done

---
+++ Ex6/Program.cs
@@ -0,0 +1,20 @@
+﻿// Написать программу показывающие первые N чисел, для которых каждое следующее равно сумме двух предыдущих. 
+// Первые два элемента последовательности задаются пользователем
+
+void SequenceNumber (int a, int b, int size)
+{
+    if (size == 0) return;
+    else 
+    {
+    Console.Write((a + b) + " ");
+    SequenceNumber (b, a+b, size-1);
+    }
+}
+
+Console.WriteLine("Enter number a:");
+int a = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Enter number b:");
+int b = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Enter number size:");
+int size = int.Parse(Console.ReadLine() ?? "0");
+SequenceNumber(a, b, size);
